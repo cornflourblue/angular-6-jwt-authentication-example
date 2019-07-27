aa123f1d582917ff2bf3df03a04a185d6cdeefc0
Ben <ben.beckers@gmail.com>
2019-07-27 11:27:07 +0200

    Update error.interceptor.ts
    location.reload(true) is deprecated in favor of location.reload().

--- src/app/_helpers/error.interceptor.ts
+++ src/app/_helpers/error.interceptor.ts
@@ -14,7 +14,7 @@ export class ErrorInterceptor implements HttpInterceptor {
             if (err.status === 401) {
                 // auto logout if 401 response returned from api
                 this.authenticationService.logout();
-                location.reload(true);
+                location.reload();
             }
             
             const error = err.error.message || err.statusText;
